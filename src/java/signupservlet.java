cddabb4c09ad5d533869f491c972b73b594e74be
parth-dixit <42636472+parth-dixit@users.noreply.github.com>
2022-03-14 04:23:45 -0400

    Update signupservlet.java

--- src/java/signupservlet.java
+++ src/java/signupservlet.java
@@ -20,10 +20,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Parth Chutiyo
- */
+
 @WebServlet(urlPatterns = {"/signupservlet"})
 public class signupservlet extends HttpServlet {
 
